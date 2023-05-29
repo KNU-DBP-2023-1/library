9239af38730ace2796c44cba6969fb7db961c825
iceprins <junghankyul@gmail.com>
2023-05-29 15:20:46 +0900

    entity 추가

---
+++ src/main/java/com/knu/library/entity/BookEntity.java
@@ -0,0 +1,26 @@
+package com.knu.library.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter @Setter
+@Entity(name = "book")
+public class BookEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String title;
+
+    @Column
+    private String publisher;
+
+    @Column
+    private String author;
+
+    @Column
+    private Boolean onRent;
+}
