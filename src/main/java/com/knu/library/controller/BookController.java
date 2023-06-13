9b12344009aea85f43d7f6e45af24dff63cf83dc
taeho <rktlrhrl531@naver.com>
2023-06-13 22:10:23 +0900

    리팩터링, 마이페이지 버튼 추가

--- src/main/java/com/knu/library/controller/BookController.java
+++ src/main/java/com/knu/library/controller/BookController.java
@@ -2,15 +2,12 @@ package com.knu.library.controller;
 
 import com.knu.library.domain.Book;
 import com.knu.library.domain.Member;
-import com.knu.library.entity.BookEntity;
 import com.knu.library.domain.UserBook;
 import com.knu.library.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
@@ -29,19 +26,8 @@ public class BookController {
     @GetMapping(value = "/bookList")
     public String list(Model model, HttpServletRequest request) {
         // === 로그인 처리 로직 시작 ===
-        HttpSession session = request.getSession(false);
-        if (session == null) { //세션 쿠키가 존재하지 않으면
-            System.out.println("[BookController] session is null");
-            return "redirect:/";
-        }
-
-        Member loginMember = (Member)session.getAttribute("loginMember");
-
-        if (loginMember == null) { //세션에 회원 데이터가 없으면
-            System.out.println("[BookController] loginMember is null");
-            return "redirect:/";
-        }
-
+        Member loginMember = getLoginMember(request);
+        if (loginMember == null) return "redirect:/";
         model.addAttribute("member", loginMember);
         // === 로그인 처리 로직 끝 ===
 
@@ -49,26 +35,34 @@ public class BookController {
         model.addAttribute("books", books);
         return "book/bookList";
     }
-    
+
     @GetMapping(value= "/myPage")
     public String userBookList(Model model, HttpServletRequest request){
+        // === 로그인 처리 로직 시작 ===
+        Member loginMember = getLoginMember(request);
+        if (loginMember == null) return "redirect:/";
+        model.addAttribute("member", loginMember);
+        // === 로그인 처리 로직 끝 ===
+
+        List<UserBook> books = bookService.findUserBooks(loginMember.getId());
+        model.addAttribute("books", books);
+        return "myPage";
+    }
+
+    private Member getLoginMember(HttpServletRequest request) {
         HttpSession session = request.getSession(false);
         if (session == null) { //세션 쿠키가 존재하지 않으면
             System.out.println("[BookController] session is null");
-            return "redirect:/";
+            return null;
         }
 
         Member loginMember = (Member)session.getAttribute("loginMember");
 
         if (loginMember == null) { //세션에 회원 데이터가 없으면
             System.out.println("[BookController] loginMember is null");
-            return "redirect:/";
+            return null;
         }
-
-        model.addAttribute("member", loginMember);
-        List<UserBook> books = bookService.findUserBooks(loginMember.getId());
-        model.addAttribute("books", books);
-        return "myPage";
+        return loginMember;
     }
 
     // 아직 미완성
