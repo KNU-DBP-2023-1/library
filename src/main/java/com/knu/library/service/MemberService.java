768d3659e1c902582c3cd5c54dc5a04e156b659a
taeho <rktlrhrl531@naver.com>
2023-06-19 19:06:43 +0900

    불필요한거 삭제

--- src/main/java/com/knu/library/service/MemberService.java
+++ src/main/java/com/knu/library/service/MemberService.java
@@ -3,19 +3,16 @@ package com.knu.library.service;
 import com.knu.library.domain.Member;
 import com.knu.library.repository.MemberMapper;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
-@Slf4j
 @Service
 public class MemberService {
     private final MemberMapper memberMapper;
 
     @Transactional
     public void save(Member member) {
-        log.info("member={}", member);
         memberMapper.save(member);
     }
 
